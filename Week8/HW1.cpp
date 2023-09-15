56dd6fe6e2c3e41bb1e37cc32d4a2cbbf72f9ec5
Wiritipon Thapkhan <81100736+zignalssss@users.noreply.github.com>
2023-09-15 18:12:05 +0700

    Add files via upload

---
+++ Week8/HW1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+struct record{
+	int value;
+	struct record *next;
+};
+struct head_end{
+	struct record *head;
+	struct record *end;
+};
+struct head_end insertList(struct record *head,struct record *end,int data){
+	struct head_end box;
+	struct record *node;
+	if(head == NULL && end == NULL){
+		head = new struct record;
+		head->value = data;
+		head->next = NULL;
+		end = head;
+	}else{
+		node = new struct record;
+		node->value = data;
+		node->next = NULL;
+		end->next = node;
+		end = node;
+	}
+	box.head = head;
+	box.end = end;
+	return box;
+}
+void printList(struct record *head){
+	struct record *p = head;
+	if(head == NULL){
+		cout << "Empty List!" << endl; 
+	}else{
+		cout << "List : ";
+		while(p){
+			cout << p->value << " ";
+			p = p->next;
+		}
+	}
+}
+int main(){
+	struct head_end box;
+	struct record *head = NULL ,*end = NULL;
+	int data;
+	cout << "Input : ";
+	while(true){
+		cin >> data;
+		if(data <= 0){
+			break;
+		}
+		box = insertList(head,end,data);
+		head = box.head;
+		end = box.end;
+	}
+	printList(head);
+
+	
+	return 0;
+}
